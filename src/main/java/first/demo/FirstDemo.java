a8dcb478584a57b7af673f3469929948c6448d62
Ibraim Asanov <iasanov@nemetschek.bg>
2024-01-23 20:20:16 +0200

    Added working with the console in the both directions

--- src/main/java/first/demo/FirstDemo.java
+++ src/main/java/first/demo/FirstDemo.java
@@ -1,7 +1,21 @@
 package first.demo;
 
+import java.util.Scanner;
+
 public class FirstDemo {
     public static void main (String [] args){
-       System.out.println("Hello World");
+        //this will print the text in the console
+        System.out.println("Please enter your name:");
+
+        //Scanner class has build in methods and ability to read user data from the console
+        Scanner scanner = new Scanner(System.in);
+
+        //defining a variable to store the user input
+        String userName = scanner.nextLine();
+
+        //printing final message
+        System.out.println("Hello " + userName + ". Nice to meet you!");
+
+
     }
 }

f863db4072cfaa1255f466c218365092958aaeb0
Ibraim Asanov <iasanov@nemetschek.bg>
2024-02-08 21:02:40 +0200

    Methods and Constructors

---
+++ src/main/java/methods/MethodsDemo.java
@@ -0,0 +1,19 @@
+package methods;
+
+public class MethodsDemo {
+    public static void main(String[] args) {
+        System.out.println(sum(4,5));
+        System.out.println(sum(3, 5, 9));
+
+        System.out.println(sum(sum(4,5,12), sum(3, 56)));
+    }
+
+    public static int sum(int a, int b){
+        return a + b;
+    }
+
+    //method overload, came from polymorphism
+    public static int sum(int a, int b, int c){
+        return a + b + c;
+    }
+}
---
+++ src/main/java/methods/Test.java
@@ -0,0 +1,7 @@
+package methods;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(MethodsDemo.sum(5, 20));
+    }
+}
